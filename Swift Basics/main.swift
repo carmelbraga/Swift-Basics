8c0f8944d3336cf129931e782d1cc17cdf2c1429
Carmel Braga <carmelbraga@mwc-115168.dhcp.missouri.edu>
2019-02-04 14:17:39 -0600

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Carmel Braga on 2/4/19.
+//  Copyright © 2019 Carmel Braga. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
